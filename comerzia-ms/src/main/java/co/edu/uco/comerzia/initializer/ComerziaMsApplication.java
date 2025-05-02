be0f8ef255a3a5f08f65cb7b820ee2d4c17ae501
JZuluf <148837724+JZuluf@users.noreply.github.com>
2025-05-01 20:54:36 -0500

    Cargar linea base
    Cargar arquetipo inicial de la linea base para comerciar a evolucionar.

---
+++ comerzia-ms/src/main/java/co/edu/uco/comerzia/initializer/ComerziaMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.comerzia.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerziaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComerziaMsApplication.class, args);
+	}
+
+}

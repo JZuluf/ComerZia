32b88498f29f7c1c3d67b4f531c59b2bd52e5a50
JZuluf <148837724+JZuluf@users.noreply.github.com>
2025-05-06 20:34:14 -0500

    Crear Dummys para carga de carpetas o paquetes del arquetipo
    Crear clases dummy para que las carpetas suban sin problema al
    repositorio.

---
+++ comerzia-ms/src/main/java/co/edu/uco/comerzia/application/interactor/dto/Dummy.java
@@ -0,0 +1,5 @@
+package co.edu.uco.comerzia.application.interactor.dto;
+
+public class Dummy {
+
+}
---
+++ comerzia-ms/src/main/java/co/edu/uco/comerzia/application/outputport/dto/Dummy.java
@@ -0,0 +1,5 @@
+package co.edu.uco.comerzia.application.outputport.dto;
+
+public class Dummy {
+
+}
